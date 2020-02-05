761d5d716bf76aec6d49b062cac00c1435044d7f
seongtaekkim <chxortnl@gmail.com>
2020-02-05 22:46:47 +0900

    deleted:    ../.classpath 	deleted:    ../.project 	deleted:    ../.settings/org.eclipse.jdt.core.prefs 	modified:   ch1_1_1/dao/UserDao.java

--- src/ch1_1_1/dao/UserDao.java
+++ src/ch1_1_1/dao/UserDao.java
@@ -13,7 +13,7 @@ public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/board","root","1234");
-		 
+		  
 		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
