4a90fb26ffc1af53f97a615a29801273ac17c101
seongtaekkim <chxortnl@gmail.com>
2020-02-06 00:46:57 +0900

    new file:   ch1_1_3_1/dao/SimpleConnectionMaker.java 	new file:   ch1_1_3_1/dao/UserDao.java 	new file:   ch1_1_3_1/domain/User.java 	new file:   ch1_1_3_2/dao/ConnectionMaker.java 	new file:   ch1_1_3_2/dao/DConnectionMaker.java 	new file:   ch1_1_3_2/dao/UserDao.java 	new file:   ch1_1_3_2/domain/User.java 	new file:   ch1_1_3_3/dao/ConnectionMaker.java 	new file:   ch1_1_3_3/dao/DConnectionMaker.java 	new file:   ch1_1_3_3/dao/UserDao.java 	new file:   ch1_1_3_3/dao/UserDaoTest.java 	new file:   ch1_1_3_3/domain/User.java

---
+++ src/ch1_1_3_3/dao/DConnectionMaker.java
@@ -0,0 +1,17 @@
+package ch1_1_3_3.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/board","root","1234");
+		return c;
+	}
+
+
+}
