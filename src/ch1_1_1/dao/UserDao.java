d88111ae8af6c526d65734d20af5059bccf417f1
unknown <chxortnl@gmail.com>
2020-02-05 22:38:52 +0900

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/ch1_1_1/dao/UserDao.class 	new file:   bin/ch1_1_1/domain/User.class 	new file:   bin/ch1_1_2/dao/UserDao.class 	new file:   bin/ch1_1_2/domain/User.class 	new file:   mysql-connector-java-5.1.44-bin.jar 	new file:   src/ch1_1_1/dao/UserDao.java 	new file:   src/ch1_1_1/domain/User.java 	new file:   src/ch1_1_2/dao/UserDao.java 	new file:   src/ch1_1_2/domain/User.java

---
+++ src/ch1_1_1/dao/UserDao.java
@@ -0,0 +1,66 @@
+package ch1_1_1.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ch1_1_1.domain.User;
+
+public class UserDao {
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/board","root","1234");
+		 
+		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/board","root","1234");
+		
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("kim2");
+		user.setName("kim");
+		user.setPassword("1234");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록성공");
+		
+		User user2 = dao.get(user.getId());
+		
+		System.out.println(user2.getId());
+		System.out.println("조회성공");
+	}
+}
