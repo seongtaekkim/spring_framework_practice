058cf1e92090405e33c654cd9a008d58b2eaeacd
seongtaekkim <chxortnl@gmail.com>
2020-03-01 23:57:20 +0900

    modified:   dao/JdbcContext.java

--- src/main/java/dao/JdbcContext.java
+++ src/main/java/dao/JdbcContext.java
@@ -3,8 +3,16 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.SqlProvider;
+import org.springframework.jdbc.core.StatementCallback;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.util.Assert;
 
 public class JdbcContext {
 	private DataSource dataSource;
@@ -39,5 +47,61 @@ public class JdbcContext {
 				}
 			}
 		);
+	}
+	
+	public int update(final String sql) throws DataAccessException, SQLException {
+		class UpdateStatementCallback implements StatementCallback<Integer>, SqlProvider {
+			@Override
+			public Integer doInStatement(Statement stmt) throws SQLException {
+				int rows = stmt.executeUpdate(sql);
+				return rows;
+			}
+			@Override
+			public String getSql() {
+				return sql;
+			}
+		}
+		return execute(new UpdateStatementCallback());
+	}
+	
+	//-------------------------------------------------------------------------
+	// Methods dealing with static SQL (java.sql.Statement)
+	//-------------------------------------------------------------------------
+
+	public <T> T execute(StatementCallback<T> action) throws DataAccessException, SQLException {
+		Assert.notNull(action, "Callback object must not be null");
+
+		Connection con = this.dataSource.getConnection();
+		Statement stmt = null;
+		try {
+			Connection conToUse = con;
+//			if (this.nativeJdbcExtractor != null &&
+//					this.nativeJdbcExtractor.isNativeConnectionNecessaryForNativeStatements()) {
+//				conToUse = this.nativeJdbcExtractor.getNativeConnection(con);
+//			}
+			stmt = conToUse.createStatement();
+		//	applyStatementSettings(stmt);
+			Statement stmtToUse = stmt;
+//			if (this.nativeJdbcExtractor != null) {
+//				stmtToUse = this.nativeJdbcExtractor.getNativeStatement(stmt);
+//			}
+			T result = action.doInStatement(stmtToUse);
+			//handleWarnings(stmt);
+			return result;
+		}
+		catch (SQLException ex) {
+			// Release Connection early, to avoid potential connection pool deadlock
+			// in the case when the exception translator hasn't been initialized yet.
+			JdbcUtils.closeStatement(stmt);
+			stmt = null;
+			//DataSourceUtils.releaseConnection(con, this.dataSource.getConnection());
+			con = null;
+			//throw getExceptionTranslator().translate("StatementCallback", getSql(action), ex);
+		}
+		finally {
+			JdbcUtils.closeStatement(stmt);
+			//DataSourceUtils.releaseConnection(con, getDataSource());
+		}
+		return null;
 	}
 }
